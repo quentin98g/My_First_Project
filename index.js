fb0380f8e93e39590c2a17fbb12ef8c734a6f92a
Quentin <quentin.98g@gmail.com>
2025-11-29 18:20:24 +0100

    Exercice Florian

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log("Hello Word !");
+const INDEPENDENCE_YEAR = 1776;
+let numberOfColonies = 13;
+let firstColony = 'Virginia';
+let estimatedPopulation1776 = 2500000;
+const isIndependent = true;
 
-console.log("Bonjour Florian !");
+console.log(INDEPENDENCE_YEAR)
+console.log(numberOfColonies)
+console.log(firstColony)
+console.log(estimatedPopulation1776)
+console.log(isIndependent)
